210c5907eab55ad1d0e63cb9f210e06d6fcd1418
kashifulhaque <haque.kashiful7@gmail.com>
2022-06-29 15:47:08 +0530

    Async

--- api/characters/[id].js
+++ api/characters/[id].js
@@ -1,6 +1,6 @@
 import { getData } from '../_utils';
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const { id } = req.query;
   const data = await getData('hp_character', id);
   return res.status(data.status).json(data);
--- api/wands.js
+++ api/wands.js
@@ -1,6 +1,6 @@
 import { getData } from './_utils';
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const data = await getData('wand');
   res.status(data.status).json(data);
 }
